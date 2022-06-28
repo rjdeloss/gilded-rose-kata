b3f0980ff0bdf8ac590707d1f099600c14ca8f88
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-06-28 11:57:55 -0400

    create test for Aged Brie. should increase in quality as @sell_in decreases

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,9 +3,20 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      expect(true).to be(true)
+    describe "Item Aged Brie" do 
+      it "should increase in quality as @sell_in decreases" do 
+        items = [Item.new("Aged Brie", 3, 3)]
+        market = GildedRose.new(items)
+
+        market.update_quality
+        aged_brie = market.items[0]
+        
+        expect(aged_brie).to have_attributes(:name => "Aged Brie", :sell_in => 2, :quality => 4)
+      end
     end
+    # it "does not change the name" do
+    #   expect(true).to be(true)
+    # end
   end
 
 end

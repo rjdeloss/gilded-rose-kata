d97eee5a967da551dd5493b7b207923bcbf84d50
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-06-28 12:19:07 -0400

    create test for Backstage Pass. It should increase in quality as @sell_in decreases

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,7 +3,7 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    describe "Item Aged Brie" do 
+    describe "Item: Aged Brie" do 
       it "should increase in quality as @sell_in decreases" do 
         items = [Item.new("Aged Brie", 3, 3)]
         market = GildedRose.new(items)
@@ -32,6 +32,18 @@ describe GildedRose do
         aged_brie = market.items[0]
 
         expect(aged_brie).to have_attributes(:name => "Aged Brie", :sell_in => 1, :quality => 50)
+      end
+    end
+
+    describe "Item: Backstage passes to a TAFKAL80ETC concert" do
+      it "should increase in quality as @sell_in decreases" do 
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 3)]
+        market = GildedRose.new(items)
+
+        market.update_quality
+        backstage_pass = market.items[0]
+
+        expect(backstage_pass).to have_attributes(:name => "Backstage passes to a TAFKAL80ETC concert", :sell_in => 10, :quality => 4)
       end
     end
 

148fc49d9a0f4dfbca73401f50aea0db517c06ae
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-06-28 12:10:03 -0400

    write test for Aged Brie. it should double in quality if it updates after @sell_in date

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -10,13 +10,21 @@ describe GildedRose do
 
         market.update_quality
         aged_brie = market.items[0]
-        
+
         expect(aged_brie).to have_attributes(:name => "Aged Brie", :sell_in => 2, :quality => 4)
       end
+      
+      it "should double in quality as @sell_in is updated after the expiration" do 
+        items = [Item.new("Aged Brie", 0, 4)]
+        market = GildedRose.new(items)
+
+        market.update_quality
+        aged_brie = market.items[0]
+
+        expect(aged_brie).to have_attributes(:name => "Aged Brie", :sell_in => -1, :quality => 6)
+      end
     end
-    # it "does not change the name" do
-    #   expect(true).to be(true)
-    # end
+
   end
 
 end
